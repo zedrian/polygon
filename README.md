3adeb0623e78e53786e441a3170d0a9c0088463d
zedrian <artsiom.kopats@gmail.com>
2015-12-18 15:29:26 +0300

    Initial stage.

---
+++ README.md
@@ -0,0 +1,7 @@
+polygon
+=======
+Repository for different experiments in programming:
+
+* separate branch for each experiment
+
+* but one repository to store'em all
---
+++ source/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+using std::cout;
+using std::endl;
+
+
+int main()
+{
+    cout << "Here is a polygon on it's MASTER branch. By default it's doing nothing but showing this message." << endl;
+}
