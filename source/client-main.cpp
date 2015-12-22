86c7ede386f8a6c94783bd613ace7691e368fffb
zedrian <artsiom.kopats@gmail.com>
2015-12-22 14:10:49 +0300

    Client code: all printf's changed to cout.

--- source/client-main.cpp
+++ source/client-main.cpp
@@ -18,23 +18,20 @@ using std::to_string;
 using std::string;
 
 
-static void my_debug(void *ctx, int level,
-                     const char *file, int line,
-                     const char *str)
+static void my_debug(void* ctx, int level,
+                     const char* file, int line,
+                     const char* str)
 {
-    ((void) level);
-
-    mbedtls_fprintf((FILE *) ctx, "%s:%04d: %s", file, line, str);
-    fflush((FILE *) ctx);
+    cout << file << ":" << line << ": " << str << endl;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     int ret, len;
     mbedtls_net_context server_fd;
     uint32_t flags;
     unsigned char buf[1024];
-    const char *pers = "dtls_client";
+    string personalizating_vector = "dtls_client";
     int retry_left = 5;
     string server_address;
 
@@ -45,8 +42,6 @@ int main(int argc, char *argv[])
     mbedtls_x509_crt cacert;
     mbedtls_timing_delay_context timer;
 
-    ((void) argc);
-    ((void) argv);
 
 #if defined(MBEDTLS_DEBUG_C)
     mbedtls_debug_set_threshold(0);
@@ -61,35 +56,30 @@ int main(int argc, char *argv[])
     mbedtls_x509_crt_init(&cacert);
     mbedtls_ctr_drbg_init(&ctr_drbg);
 
-    mbedtls_printf("\n  . Seeding the random number generator...");
-    fflush(stdout);
+    cout << "Seeding the random number generator: ";
 
     mbedtls_entropy_init(&entropy);
-    if ((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
-                                     (const unsigned char *) pers,
-                                     strlen(pers))) != 0)
+    if ((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, (const unsigned char*) personalizating_vector.data(), personalizating_vector.size())) != 0)
     {
-        mbedtls_printf(" failed\n  ! mbedtls_ctr_drbg_seed returned %d\n", ret);
+        cout << "failed: mbedtls_ctr_drbg_seed() returned " << ret << endl;
         goto exit;
     }
 
-    mbedtls_printf(" ok\n");
+    cout << "success" << endl;
 
     /*
      * 0. Load certificates
      */
-    mbedtls_printf("  . Loading the CA root certificate ...");
-    fflush(stdout);
+    cout << "Loading the CA root certificate: ";
 
-    ret = mbedtls_x509_crt_parse(&cacert, (const unsigned char *) mbedtls_test_cas_pem,
-                                 mbedtls_test_cas_pem_len);
+    ret = mbedtls_x509_crt_parse(&cacert, (const unsigned char*) mbedtls_test_cas_pem, mbedtls_test_cas_pem_len);
     if (ret < 0)
     {
-        mbedtls_printf(" failed\n  !  mbedtls_x509_crt_parse returned -0x%x\n\n", -ret);
+        cout << "failed: mbedtls_x509_crt_parse() returned " << ret << endl;
         goto exit;
     }
 
-    mbedtls_printf(" ok (%d skipped)\n", ret);
+    cout << "success (" << ret << " skipped)" << endl;
 
     /*
      * 1. Start the connection
@@ -99,29 +89,24 @@ int main(int argc, char *argv[])
     cout << "Enter server port: ";
     unsigned int port;
     cin >> port;
-    cout << "  . Connecting to UDP " << server_address<< ":" << port << "...";
+    cout << "Connecting to server: ";
 
-    if ((ret = mbedtls_net_connect(&server_fd, server_address.data(),
-                                   to_string(port).data(), MBEDTLS_NET_PROTO_UDP)) != 0)
+    if ((ret = mbedtls_net_connect(&server_fd, server_address.data(), to_string(port).data(), MBEDTLS_NET_PROTO_UDP)) != 0)
     {
-        mbedtls_printf(" failed\n  ! mbedtls_net_connect returned %d\n\n", ret);
+        cout << "failed: mbedtls_net_connect() returned " << ret << endl;
         goto exit;
     }
 
-    mbedtls_printf(" ok\n");
+    cout << "success" << endl;
 
     /*
      * 2. Setup stuff
      */
-    mbedtls_printf("  . Setting up the DTLS structure...");
-    fflush(stdout);
+    cout << "Setting up the DTLS structure: ";
 
-    if ((ret = mbedtls_ssl_config_defaults(&conf,
-                                           MBEDTLS_SSL_IS_CLIENT,
-                                           MBEDTLS_SSL_TRANSPORT_DATAGRAM,
-                                           MBEDTLS_SSL_PRESET_DEFAULT)) != 0)
+    if ((ret = mbedtls_ssl_config_defaults(&conf, MBEDTLS_SSL_IS_CLIENT, MBEDTLS_SSL_TRANSPORT_DATAGRAM, MBEDTLS_SSL_PRESET_DEFAULT)) != 0)
     {
-        mbedtls_printf(" failed\n  ! mbedtls_ssl_config_defaults returned %d\n\n", ret);
+        cout << "failed: mbedtls_ssl_config_defaults() returned " << ret << endl;
         goto exit;
     }
 
@@ -135,46 +120,43 @@ int main(int argc, char *argv[])
 
     if ((ret = mbedtls_ssl_setup(&ssl, &conf)) != 0)
     {
-        mbedtls_printf(" failed\n  ! mbedtls_ssl_setup returned %d\n\n", ret);
+        cout << "failed: mbedtls_ssl_setup() returned " << ret << endl;
         goto exit;
     }
 
     if ((ret = mbedtls_ssl_set_hostname(&ssl, "localhost")) != 0)
     {
-        mbedtls_printf(" failed\n  ! mbedtls_ssl_set_hostname returned %d\n\n", ret);
+        cout << "failed: mbedtls_ssl_set_hostname() returned " << ret << endl;
         goto exit;
     }
 
-    mbedtls_ssl_set_bio(&ssl, &server_fd,
-                        mbedtls_net_send, mbedtls_net_recv, mbedtls_net_recv_timeout);
+    mbedtls_ssl_set_bio(&ssl, &server_fd, mbedtls_net_send, mbedtls_net_recv, mbedtls_net_recv_timeout);
 
-    mbedtls_ssl_set_timer_cb(&ssl, &timer, mbedtls_timing_set_delay,
-                             mbedtls_timing_get_delay);
+    mbedtls_ssl_set_timer_cb(&ssl, &timer, mbedtls_timing_set_delay, mbedtls_timing_get_delay);
 
-    mbedtls_printf(" ok\n");
+    cout << "success" << endl;
 
     /*
      * 4. Handshake
      */
-    mbedtls_printf("  . Performing the SSL/TLS handshake...");
-    fflush(stdout);
+    cout << "Performing the SSL/TLS handshake: ";
 
-    do ret = mbedtls_ssl_handshake(&ssl);
-    while (ret == MBEDTLS_ERR_SSL_WANT_READ ||
-           ret == MBEDTLS_ERR_SSL_WANT_WRITE);
+    do
+        ret = mbedtls_ssl_handshake(&ssl);
+    while (ret == MBEDTLS_ERR_SSL_WANT_READ || ret == MBEDTLS_ERR_SSL_WANT_WRITE);
 
     if (ret != 0)
     {
-        mbedtls_printf(" failed\n  ! mbedtls_ssl_handshake returned -0x%x\n\n", -ret);
+        cout << "failed: mbedtls_ssl_handshake() returned " << ret << endl;
         goto exit;
     }
 
-    mbedtls_printf(" ok\n");
+    cout << "success" << endl;
 
     /*
      * 5. Verify the server certificate
      */
-    mbedtls_printf("  . Verifying peer X.509 certificate...");
+    cout << "Verifying peer X.509 certificate: ";
 
     /* In real life, we would have used MBEDTLS_SSL_VERIFY_REQUIRED so that the
      * handshake would not succeed if the peer's cert is bad.  Even if we used
@@ -183,86 +165,84 @@ int main(int argc, char *argv[])
     {
         char vrfy_buf[512];
 
-        mbedtls_printf(" failed\n");
+        cout << "failed: ";
 
-        mbedtls_x509_crt_verify_info(vrfy_buf, sizeof(vrfy_buf), "  ! ", flags);
+        mbedtls_x509_crt_verify_info(vrfy_buf, sizeof(vrfy_buf), "", flags);
 
-        mbedtls_printf("%s\n", vrfy_buf);
+        cout << vrfy_buf << endl;
     }
     else
-        mbedtls_printf(" ok\n");
+        cout << "success" << endl;
 
     /*
      * 6. Write the echo request
      */
     send_request:
-    mbedtls_printf("  > Write to server:");
-    fflush(stdout);
+    cout << "Sending to server: ";
 
     len = sizeof("message") - 1;
 
-    do ret = mbedtls_ssl_write(&ssl, (unsigned char *) "message", len);
-    while (ret == MBEDTLS_ERR_SSL_WANT_READ ||
-           ret == MBEDTLS_ERR_SSL_WANT_WRITE);
+    do
+        ret = mbedtls_ssl_write(&ssl, (unsigned char*) "message", len);
+    while (ret == MBEDTLS_ERR_SSL_WANT_READ || ret == MBEDTLS_ERR_SSL_WANT_WRITE);
 
     if (ret < 0)
     {
-        mbedtls_printf(" failed\n  ! mbedtls_ssl_write returned %d\n\n", ret);
+        cout << "failed: mbedtls_ssl_write() returned " << ret << endl;
         goto exit;
     }
 
     len = ret;
-    mbedtls_printf(" %d bytes written\n\n%s\n\n", len, "message");
+    cout << len << " bytes written: " << "message" << endl;
 
     /*
      * 7. Read the echo response
      */
-    mbedtls_printf("  < Read from server:");
-    fflush(stdout);
+    cout << "Receiving from server: ";
 
     len = sizeof(buf) - 1;
     memset(buf, 0, sizeof(buf));
 
-    do ret = mbedtls_ssl_read(&ssl, buf, len);
-    while (ret == MBEDTLS_ERR_SSL_WANT_READ ||
-           ret == MBEDTLS_ERR_SSL_WANT_WRITE);
+    do
+        ret = mbedtls_ssl_read(&ssl, buf, len);
+    while (ret == MBEDTLS_ERR_SSL_WANT_READ || ret == MBEDTLS_ERR_SSL_WANT_WRITE);
 
     if (ret <= 0)
     {
         switch (ret)
         {
             case MBEDTLS_ERR_SSL_TIMEOUT:
-                mbedtls_printf(" timeout\n\n");
+                cout << "timeout" << endl;
                 if (retry_left-- > 0)
                     goto send_request;
                 goto exit;
 
             case MBEDTLS_ERR_SSL_PEER_CLOSE_NOTIFY:
-                mbedtls_printf(" connection was closed gracefully\n");
+                cout << "connection was closed gracefully" << endl;
                 ret = 0;
                 goto close_notify;
 
             default:
-                mbedtls_printf(" mbedtls_ssl_read returned -0x%x\n\n", -ret);
+                cout << "mbedtls_ssl_read() returned " << ret << endl;
                 goto exit;
         }
     }
 
     len = ret;
-    mbedtls_printf(" %d bytes read\n\n%s\n\n", len, buf);
+    cout << len << " bytes received: " << buf << endl;
 
     /*
      * 8. Done, cleanly close the connection
      */
     close_notify:
-    mbedtls_printf("  . Closing the connection...");
+    cout << "Closing the connection: ";
 
     /* No error checking, the connection might be closed already */
     do ret = mbedtls_ssl_close_notify(&ssl);
     while (ret == MBEDTLS_ERR_SSL_WANT_WRITE);
     ret = 0;
 
-    mbedtls_printf(" done\n");
+    cout << "success" << endl;
 
     /*
      * 9. Final clean-ups and exit
@@ -274,7 +254,7 @@ int main(int argc, char *argv[])
     {
         char error_buf[100];
         mbedtls_strerror(ret, error_buf, 100);
-        mbedtls_printf("Last error was: %d - %s\n\n", ret, error_buf);
+        cout << "Last error was: " << ret << " - " << error_buf << endl;
     }
 #endif
 
@@ -287,8 +267,7 @@ int main(int argc, char *argv[])
     mbedtls_entropy_free(&entropy);
 
 #if defined(_WIN32)
-    mbedtls_printf("  + Press Enter to exit this program.\n");
-    fflush(stdout);
+    cout << "Press Enter to exit" << endl;
     getchar();
 #endif
 
