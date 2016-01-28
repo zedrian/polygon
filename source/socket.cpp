3e8cb4561a7bcf3be87013ece1998e6901f4c762
zedrian <artsiom.kopats@gmail.com>
2016-01-28 20:10:11 +0300

    Socket: useless couts removed.

--- source/socket.cpp
+++ source/socket.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <stdexcept>
 
 #include <mbedtls/timing.h>
@@ -17,9 +16,6 @@ Socket::Socket()
     int ret;
     mbedtls_debug_set_threshold(0);
 
-    /*
-     * 0. Initialize the RNG and the session data
-     */
     mbedtls_net_init(&_net_context);
     mbedtls_ssl_init(&_ssl_context);
     mbedtls_ssl_config_init(&_ssl_configuration);
@@ -27,19 +23,12 @@ Socket::Socket()
     mbedtls_ctr_drbg_init(&_ctr_drbg_context);
     mbedtls_entropy_init(&_entropy_context);
 
-    cout << "Seeding the random number generator: ";
     string personalizing_vector = "Socket, constructed by itself at the moment = " + to_string(mbedtls_timing_hardclock());
     if ((ret = mbedtls_ctr_drbg_seed(&_ctr_drbg_context, mbedtls_entropy_func, &_entropy_context, (const unsigned char*) personalizing_vector.data(), personalizing_vector.size())) != 0)
         throw runtime_error(constructErrorMessage("mbedtls_ctr_drbg_seed()", ret));
-    cout << "success" << endl;
 
-    /*
-     * 0. Load certificates
-     */
-    cout << "Loading the CA root certificate: ";
     if ((ret = mbedtls_x509_crt_parse(&_certificate, (const unsigned char*) mbedtls_test_cas_pem, mbedtls_test_cas_pem_len)) < 0)
         throw runtime_error(constructErrorMessage("mbedtls_x509_crt_parse()", ret));
-    cout << "success (" << ret << " skipped)" << endl;
 
     _constructed_by_acceptor = false;
     _last_sent_message_id = -1;
@@ -73,7 +62,6 @@ Socket::Socket(mbedtls_net_context net_context,
 
 Socket::~Socket()
 {
-    cout << "Release: ";
     mbedtls_net_free(&_net_context);
     mbedtls_ssl_free(&_ssl_context);
     mbedtls_ctr_drbg_free(&_ctr_drbg_context);
@@ -84,8 +72,6 @@ Socket::~Socket()
         mbedtls_ssl_config_free(&_ssl_configuration);
         mbedtls_x509_crt_free(&_certificate);
     }
-
-    cout << "success" << endl;
 }
 
 
@@ -96,13 +82,10 @@ void Socket::connect(const string address,
         throw logic_error("Can't connect socket that is already connected.");
 
     int ret;
-    cout << "Connecting to server: ";
 
     if ((ret = mbedtls_net_connect(&_net_context, address.data(), to_string(port).data(), MBEDTLS_NET_PROTO_UDP)) != 0)
         throw runtime_error(constructErrorMessage("mbedtls_net_connect()", ret));
-    cout << "success" << endl;
 
-    cout << "Setting up the DTLS structure: ";
     ret = mbedtls_ssl_config_defaults(&_ssl_configuration, MBEDTLS_SSL_IS_CLIENT, MBEDTLS_SSL_TRANSPORT_DATAGRAM, MBEDTLS_SSL_PRESET_DEFAULT);
     if (ret != 0)
         throw runtime_error(constructErrorMessage("mbedtls_ssl_config_defaults()", ret));
@@ -123,18 +106,14 @@ void Socket::connect(const string address,
 
     mbedtls_ssl_set_bio(&_ssl_context, &_net_context, mbedtls_net_send, mbedtls_net_recv, mbedtls_net_recv_timeout);
     mbedtls_ssl_set_timer_cb(&_ssl_context, &_delay_context, mbedtls_timing_set_delay, mbedtls_timing_get_delay);
-    cout << "success" << endl;
 
-    cout << "Performing the SSL/TLS handshake: ";
     do
         ret = mbedtls_ssl_handshake(&_ssl_context);
     while (ret == MBEDTLS_ERR_SSL_WANT_READ || ret == MBEDTLS_ERR_SSL_WANT_WRITE);
 
     if (ret != 0)
         throw runtime_error(constructErrorMessage("mbedtls_ssl_handshake()", ret));
-    cout << "success" << endl;
 
-    cout << "Verifying peer X.509 certificate: ";
     /* In real life, we would have used MBEDTLS_SSL_VERIFY_REQUIRED so that the
      * handshake would not succeed if the peer's cert is bad.  Even if we used
      * MBEDTLS_SSL_VERIFY_OPTIONAL, we would bail out here if ret != 0 */
@@ -146,7 +125,6 @@ void Socket::connect(const string address,
 
         throw runtime_error(verifying_buffer);
     }
-    cout << "success" << endl;
 
     mbedtls_timing_get_timer(&_clock, 1);
     _connected = true;
@@ -220,8 +198,6 @@ size_t Socket::receive(unsigned char* buffer,
     for (size_t i = 0; i < message.bytes().size() - sizeof(Header); ++i)
         buffer[i] = message.data()[i];
 
-    cout << endl << "Received at local time = " << mbedtls_timing_get_timer(&_clock, 0) << endl;
-    cout << "Construction time = " << message.header().construction_time() << endl;
     return bytes_received - sizeof(Header);
 }
 
@@ -277,11 +253,9 @@ size_t Socket::receiveMessage(Message& message,
     switch (bytes_received)
     {
         case MBEDTLS_ERR_SSL_TIMEOUT:
-            cout << "timeout" << endl;
             return 0;
 
         case MBEDTLS_ERR_SSL_PEER_CLOSE_NOTIFY:
-            cout << "connection was closed gracefully" << endl;
             _connected = false;
             return 0;
 
